97f5066a3d118de030fc4f25cdb38a732333c052
russellocarlo <russello.carlo99@gmail.com>
2024-11-25 18:03:05 +0100

    Update Test.java

--- src/main/java/org/elis/utility/Test.java
+++ src/main/java/org/elis/utility/Test.java
@@ -5,9 +5,6 @@ import org.elis.dao.jpa.JPADaoFactory;
 
 public class Test {
 	public static void main(String[] args) {
-		//JPADaoFactory.getEntityManager();
-		//System.out.println(BusinessLogic.getGeneri());
-		//web.xml potrebbe dare problemi per aggiorna recensione servlet
-		//System.out.println(BusinessLogic.getUtenti());
+		
 	}
 }
